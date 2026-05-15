7410fb5c1e2774be62d515897f25091ee8cda760
Antonio Gutierrez <ventas.ejecutivob2b@gmail.com>
2026-05-14 19:46:47 -0500

    Fix: Explicitly resolve index.html in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
